62960950e8757340d5b3ad480be333865fccc0b1
Leusiojosefilimone <leusiojosefilimone@gmail.com>
2023-01-16 14:03:18 -0300

    req_params_query_body

--- 10_javascript_node_mongoDB/03_node_express/02_express_req/server.js
+++ 10_javascript_node_mongoDB/03_node_express/02_express_req/server.js
@@ -1,14 +1,41 @@
 const express = require('express')
-console.log(express)
 const app = express()
+app.use(
+    express.urlencoded(
+        {
+            extended: true
+        }
+    )
+)
 
 app.get('/', (req, res) => {
-    res.send('Hello <b>world</b>!');
+    res.send(`<form action="/" method="POST">
+    Nome do cliente: <input type="text" name="name"></br>
+    Empresa do cliente: <input type="text" name="empresa">
+    <button>Olá Mundo</button>
+</form>`);
 })
+
+app.get('/testes/:idUsuarios?', (req, res) => {
+    console.log(req.params.idUsuarios)
+    console.log(req.query.nome)
+    res.send('Oi ' + req.params.idUsuarios)
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send(`<p>O teu nome é ${ req.body.name}</p>
+    <p>A tua empresa é ${ req.body.empresa}</p>`)
+})
+
+app.post('/', (req, res) => {
+    res.send('Ola Mundo')
+})
+
 app.get('/contacto', (req, res) => {
     res.send('Hello <b>obrigado</b> por entrar em contacto conosco!');
 })
 
-app.listen(3100,() => {
-    console.log('servidor executando na porta 3100')
+app.listen(3800,() => {
+    console.log('servidor executando na porta 3800')
 })
